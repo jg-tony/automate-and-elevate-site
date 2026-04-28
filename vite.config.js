9132e18a7b167e938652e910e59a6315a5efa943
jg-tony <tony@automateandelevatebiz.com>
2026-04-27 20:45:40 -0500

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' if using custom domain
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.js',
-  },
+  base: '/automate-and-elevate-site/',
 })
